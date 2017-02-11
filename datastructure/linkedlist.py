fe3d9c24e2640fbbcc92c37b21272b59a9f45426
zhiruchen <zhiruchen1992@foxmail.com>
2017-02-11 22:34:37 +0800

    反转单链表

--- datastructure/linkedlist.py
+++ datastructure/linkedlist.py
@@ -35,3 +35,40 @@ class LinkedList(object):
         self._head = temp
         self._size += 1
 
+    def reverse(self):
+        """反转链表"""
+        cur = None
+
+        while self._head:
+            temp = self._head.next
+            self._head.next = cur
+            cur = self._head
+            self._head = temp
+
+        self._head = cur
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._head:
+            temp_head = self._head
+            self._head = self._head.next
+            return temp_head.data
+
+
+def test_link_list():
+    linked_list = LinkedList()
+    for n in range(5):
+        linked_list.add(n)
+
+    # while linked_list._head:
+    #     print linked_list._head.data
+    #     linked_list._head = linked_list._head.next
+
+    linked_list.reverse()
+    while linked_list._head:
+        print linked_list._head.data
+        linked_list._head = linked_list._head.next
+    # for x in linked_list:
+    #     print x
