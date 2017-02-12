c0e791198f042d85c12f86daf3f6932dcc9e3483
zhiruchen <zhiruchen1992@foxmail.com>
2017-02-12 11:33:50 +0800

    reverse words

--- ds_application/rotate_array.py
+++ ds_application/rotate_array.py
@@ -12,6 +12,12 @@ def reverse(array, start, end):
         end -= 1
 
 
+def reverse_words(words):
+    words_array = words.split()
+    reverse(words_array, 0, len(words_array)-1)
+    return ' '.join(words_array)
+
+
 def rotate_array(array, n):
     """
     :param array:
@@ -27,3 +33,8 @@ def test_rotate():
     mlist = [1, 2, 3, 4, 5]
     rotate_array(mlist, 2)  # [3, 4, 5, 1, 2]
     print mlist
+
+
+def test_reverse_words():
+    print reverse_words("I am a programmer")
+    print reverse_words("You are a b***h")
